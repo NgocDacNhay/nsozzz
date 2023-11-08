1ad023e41e9a72240673eaffe74227232ed5a491
unknown <dacdzcojsai@gmail.com>
2023-11-08 17:03:04 +0700

    first commit

---
+++ src/main/java/com/nsoz/constants/CMDConfirmPopup.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nsoz.constants;
+
+/**
+ *
+ * @author Admin
+ */
+public class CMDConfirmPopup {
+
+    public static final int CANCEL = 0;
+    public static final int CONFIRM = 1;
+    public static final int NANG_MAT = 2;
+    public static final int NANG_MAT_VIP = 3;
+    public static final int UPGRADE_SOUL_STONE = 5;
+    public static final int NHIEM_VU_DANH_VONG = 4;
+    public static final int NANG_BI_KIP = 100;
+    public static final int NANG_AN_TOC = 6;
+    public static final int CHANGE_USERNAME = 101;
+    public static final int NHAN_BI_KIP = 102;
+    public static final int KHAI_HOA_TAN_CONG = 10;
+    public static final int KHAI_HOA_PHONG_THU = 11;
+    public static final int KHAI_HOA_PHUC_SINH = 12;
+    public static final int KHAI_HOA_NANG_CAP = 13;
+    public static final int THANG_NGUONG = 14;
+}
